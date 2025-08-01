848b550cbc553b04f62d79d7c887ce6a36f295cf
amosogra <amosogra@gmail.com>
2025-08-01 04:16:04 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "3.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.5.0"),
-        .package(url: "https://github.com/ameshkov/swift-psl", "1.1.0"..<"2.0.0"),
+        .package(url: "https://github.com/amosogra/swift-psl", exact: "v2.0.0"),
     ],
     targets: [ 
         .target(
